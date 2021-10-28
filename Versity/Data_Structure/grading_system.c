91e6ad62ec4de6c336771bbd26cc8ef839cf3649
Abu Bakar Siddik <absiddik718@gmail.com>
2021-10-28 20:48:21 +0600

    init

--- Versity/Data_Structure/grading_system.c
+++ Versity/Data_Structure/grading_system.c
@@ -1,3 +1,7 @@
+/**
+ * Name : Abu Bakar Siddik(City University 53-Batch,Dhaka)
+ * ID : 2015302505
+ **/
 #include <stdio.h>
 struct marks{
     int Final,Mid,Quiz1,Quiz2,Quiz3,Attendance,Assignment,Total_marks,id;
@@ -76,9 +80,70 @@ int main()
                 int x = std[j].id,m = std[j].Total_marks;
                 double y = std[j].grade;
                 printf("At Position = %d Is %s Id No = %d With Grade = %.2lf and marks = %d\n",i+1,std[j].name,x,y,m);
+                std[j].Total_marks = 0;
                 break;
             }
         }
     }
     return 0;
 }
+/**
+ * Input : 
+    Enter The Number Of Students : 5
+    Enter Marks OF 1 student : 
+    Enter ID : 2015302501
+    Enter Name : A
+    Final Exam Marks : 38
+    Mid Exam Marks : 25
+    Attendance : 8
+    Assignment : 9
+    1st Quiz : 9
+    2nd Quiz : 10
+    3rd Quiz : 8
+    Enter Marks OF 2 student : 
+    Enter ID : 2015302502
+    Enter Name : B
+    Final Exam Marks : 38
+    Mid Exam Marks : 27
+    Attendance : 10
+    Assignment : 8
+    1st Quiz : 7
+    2nd Quiz : 5
+    3rd Quiz : 6
+    Enter Marks OF 3 student : 
+    Enter ID : 2015302503
+    Enter Name : C
+    Final Exam Marks : 39
+    Mid Exam Marks : 24
+    Attendance : 10
+    Assignment : 10
+    1st Quiz : 10
+    2nd Quiz : 10
+    3rd Quiz : 10
+    Enter Marks OF 4 student : 
+    Enter ID : 2015302504
+    Enter Name : D
+    Final Exam Marks : 40
+    Mid Exam Marks : 30
+    Attendance : 10
+    Assignment : 10
+    1st Quiz : 10
+    2nd Quiz : 10
+    3rd Quiz : 10
+    Enter Marks OF 5 student : 
+    Enter ID : 2015302505
+    Enter Name : E
+    Final Exam Marks : 35
+    Mid Exam Marks : 26
+    Attendance : 7
+    Assignment : 8
+    1st Quiz : 9
+    2nd Quiz : 7
+    3rd Quiz : 5
+ *Output:
+    At Position = 1 Is D Id No = 2015302504 With Grade = 4.00 and marks = 100
+    At Position = 2 Is C Id No = 2015302503 With Grade = 4.00 and marks = 93
+    At Position = 3 Is A Id No = 2015302501 With Grade = 4.00 and marks = 90
+    At Position = 4 Is B Id No = 2015302502 With Grade = 4.00 and marks = 90
+    At Position = 5 Is E Id No = 2015302505 With Grade = 4.00 and marks = 85
+*/
