3b6169ef2e9e6641bc3a344472e1a5a2df74e7c6
Abu Bakar Siddik <absiddik718@gmail.com>
2021-11-25 12:40:50 +0600

    init

--- Other/Java/hello.java
+++ Other/Java/hello.java
@@ -1,5 +1,9 @@
+import java.utilSccaner;
 public class hello {
     public static void main(String[] args) {
-        
+        Sccaner sc = new Sccaner(System.in);
+        //System.out.println("HELLO WORLD");
+        Long n = sc.nextLong();
+        System.out.println(n);
     }
 }
--- debug/1.cpp
+++ debug/1.cpp
@@ -1,29 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-void printSubsequences(vector<int> arr, int index,vector<int> subarr){
-    if (index == arr.size()){
-        int l = subarr.size();
-        if (l != 0){
-            int AND = subarr[0];
-            for(int i = 1;i<l;l++)AND &= subarr[i];
-            cout << "AND = "<< AND << '\n';
-        }
-    }
-    else{
-        printSubsequences(arr, index + 1, subarr);
-        subarr.push_back(arr[index]);
-        printSubsequences(arr, index + 1, subarr);
-    }
-    return;
-}
+
 int main(){
-    int n;cin >> n;
-    int x = (1LL << n);
-    vector<int> v(2*n),a; 
-    for(int i = 1;i<=n;i++){
-        v[i-1] = --x;
-    }
-    for(int i = 0;i<n;i++)v.push_back(i);
-    printSubsequences(v,0,a);
+   
     return 0;
 }
