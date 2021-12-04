bc664140c7761f582e624980c75b4735fc37bf9e
Abu Bakar Siddik <absiddik718@gmail.com>
2021-12-04 19:12:17 +0600

    modified

--- CodeForces/codeforces.cpp
+++ CodeForces/codeforces.cpp
@@ -10,8 +10,8 @@
 #define pf push_front
 #define all(x) x.begin(), x.end()
 #define Auto(it, v) for (auto &it : v)
-#define rep(i, n) for (int i = 0; i < (n); i++)
-#define Rep(i, a, n) for (int i = a; i < (n); i++)
+#define FOR(i, n) for (int i = 0; i < (n); i++)
+#define For(i, a, n) for (int i = a; i < (n); i++)
 
 using ll = long long int;
 using namespace std;
@@ -29,16 +29,17 @@ int CountBit(ll n) { return __builtin_popcountll(n); }
 ll n, m, p, q, l, r, k, x, y, z, ans, sum;
 
 void solve(){
-   
+    
 }
 int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
+    freopen("output.txt","w",stdout);
     #endif
     ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     int t;cin >> t;
-    Rep(i, 1, t + 1)
+    For(i,1,t+1)
         //cout << "Case #" << i << ": ",
          solve();
     return 0;
--- debug/2.cpp
+++ debug/2.cpp
@@ -1,67 +1,44 @@
-#include<bits/stdc++.h>
+/***
+ * Author: dead_end007
+ ***/
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define all(x) x.begin(), x.end()
+#define Auto(it, v) for (auto &it : v)
+#define FOR(i, n) for (int i = 0; i < (n); i++)
+#define For(i, a, n) for (int i = a; i < (n); i++)
+
+using ll = long long int;
 using namespace std;
-vector <int> v;
-int n;
-int Bainary_Search(int x){
-	int l = 0,r = n-1;
-	while(r >= l){
-		int m = (l+r)/2;
-		if(v[m] == x)
-			return m;
-		if(v[m] > x){
-			r = m - 1;
-		}else{
-			l = m + 1;
-		}
-	}
-	return -1;
-}
-int lowerbound(int x){
-	int l = 0,r = n-1,ans = 0;
-	while(r >= l){
-		int m = (l+r)/2;
-		if(v[m] > x){
-			r = m - 1;
-		}else{
-			ans = m + 1;
-			l = m + 1;
-		}
-	}
-	return ans;
-}
-int upperbound(int x){
-	int l = 0,r = n-1,ans = n+1;
-	while(r>=l){
-		int m = (l+r)/2;
-		if(v[m] >= x){
-			ans = m + 1;
-			r = m - 1;
-		}else{
-			l = m + 1;
-		}
-	}
-	return ans;
+
+const int inf = 1e9 + 7;
+const ll INF = 1e18 + 10;
+
+int SetBit(ll n, ll X) { return n | (1LL << X); }
+int ClearBit(ll n, ll X) { return n & ~(1LL << X); }
+int ToggleBit(ll n, ll X) { return n ^ (1LL << X); }
+bool CheckBit(ll n, ll X) { return (bool)(n & (1LL << X)); }
+bool isPowerOf2(ll X) {return X && ( !(X & (X-1) ) ) ; }
+int CountBit(ll n) { return __builtin_popcountll(n); }
+
+ll n, m, p, q, l, r, k, x, y, z, ans, sum;
+
+void solve(){
+
 }
 int main(){
-	ios_base::sync_with_stdio(false);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    #endif
+    ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
-	cin >> n;
-	v.resize(n);
-	for(int i = 0;i<n;i++)cin >> v[i];
-	sort(v.begin(),v.end());
-	int k;
-	cin >> k;
-	while(k--){
-		int l,r,m;
-		cin >> l >> r >> m;
-		int x = upperbound(l);
-		int y = lowerbound(r);
-		cout << x << " " << y << "\n";
-		if(Bainary_Search(m) != -1){
-			cout << "YES\n";
-		}else{
-			cout << "NO\n";
-		}
-	}
-	return 0;
+    int t;cin >> t;
+    For(i,1,t+1)
+        //cout << "Case #" << i << ": ",
+         solve();
+    return 0;
 }
