8468e95c03f47b7a798f5d89e013302cdf95a117
Abu Bakar Siddik <absiddik718@gmail.com>
2021-12-07 21:40:43 +0600

    mod

--- Other/Java/hello.java
+++ Other/Java/hello.java
@@ -1,9 +1,5 @@
-import java.utilSccaner;
 public class hello {
     public static void main(String[] args) {
-        Sccaner sc = new Sccaner(System.in);
-        //System.out.println("HELLO WORLD");
-        Long n = sc.nextLong();
-        System.out.println(n);
+        System.out.println("HELLO WORLD");
     }
 }
