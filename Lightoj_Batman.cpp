8eedb8e7a47170b109b8379dd76d2fd77013f810
Abu Bakar Siddik <absiddik718@gmail.com>
2022-01-13 22:44:58 +0600

    m

--- Lightoj_Batman.cpp
+++ Lightoj_Batman.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 int lcs(string a,string b,string c){
     int n = a.size(),m = b.size(),p = c.size();
-    int dp[n+1][m+1][p+1];
-    for(int i = 0;i<=n;i++){
-        for(int j = 0;j<=m;j++){
-            for(int k = 0;k<=p;k++){
-                if(i == 0 || j == 0 || k == 0){
-                    dp[i][j][k] = 0;
-                }else if(a[i-1] == b[j-1] && a[i-1] == c[k-1]){
+    vector <vector <vector<int> >> dp (n+1,vector<vector<int>>(m+1,vector<int>(p+1)));
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=m;j++){
+            for(int k = 1;k<=p;k++){
+                if(a[i-1] == b[j-1] && a[i-1] == c[k-1]){
                     dp[i][j][k] = dp[i-1][j-1][k-1]+1;
                 }else{
                     dp[i][j][k] = max({dp[i-1][j][k],dp[i][j-1][k],dp[i][j][k-1]});
@@ -23,8 +21,7 @@ int main(){
     for(int i = 1;i<=test;i++){
         string a,b,c;
         cin >> a >> b >> c;
-        cout << "Case " << i << ": ";
-        cout << lcs(a,b,c) << endl;
+        cout << "Case " << i << ": " << lcs(a,b,c) << endl;
     }
     return 0;
 }
