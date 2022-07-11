1bd82797a0ec77606926e66feb062a090d5a73b8
Siddik <absiddik718@gmail.com>
2022-07-11 14:43:23 +0600

    i

--- Practice/A.cpp
+++ Practice/A.cpp
@@ -2,14 +2,13 @@
 #define pb push_back
 #define all(var) var.begin(), var.end()
 #define rall(var) var.rbegin(), var.rend()
-#define rep(i, a, n) for (int i = (a); i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
 using ll = long long;
+using ld = long double;
 using namespace std;
+const ll inf = 1e9+7;
+const ll Inf = 1e18;
 void solve(){
-    int n;cin>>n;
-    vector<int>b(n);
-    for(auto &i : b)cin>>i;
-    
 }
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);
@@ -17,6 +16,8 @@ int main(){
     freopen("in.txt","r",stdin); 
     #endif
     int t;cin>>t;
-    while(t--)solve();
+    while(t--){
+        solve();
+    }
     return 0;
 }
--- Practice/A.java
+++ Practice/A.java
@@ -1,8 +1,34 @@
 import java.io.*;
 import java.util.*;
-class A{
+import javafx.util.Pair;
+public class A{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        while(t-->0){
+            String w,ans="";
+            w = input.next();
+            long p = input.nextLong();
+            ArrayList<Pair<Character,Integer>> tmp = new ArrayList<Pair<Character,Integer>>();
+            for(int i=0;i<w.length();i++){
+                tmp.add(new Pair<Character,Integer>(w.charAt(i),i));
+            }
+            Collections.sort(tmp,(a,b)->a.getValue()-b.getValue());
+            long cur = 0;
+            for(int i=0;i<tmp.size();i++){
+                cur += (tmp.get(i).getKey()-'a'+1);
+                if(cur>p){
+                    cur -= (tmp.get(i).getKey()-'a'+1);
+                    w.setcharAt(tmp.get(i).getValue(),'#');
+                }
+            }
+            for(int i=0;i<w.length();i++){
+                if(w.charAt(i)!='#'){
+                    ans += w.charAt(i);
+                }
+            }
+            System.out.println(ans);
+        }
         input.close();
     }
 }
