fe3658af6c430c76ae87c3be67e2d7da79007b8f
Siddik <absiddik718@gmail.com>
2022-07-18 01:27:35 +0600

    init

--- Atcoder/atcoder.cpp
+++ Atcoder/atcoder.cpp
@@ -1,13 +1,16 @@
 #include <bits/stdc++.h>
+#define all(x) x.begin(),x.end()
 #define rep(i,n) for(int i = 0;i<n;i++)
 using ll = long long int;
 using ld = long double;
 using namespace std;
 void solve(){
-
+    
 }
 int main(){
     cin.tie(0)->sync_with_stdio(0);
+    int t;cin>>t;
+    while(t--)
     solve();
     return 0;
 }
--- Practice/A.cpp
+++ Practice/A.cpp
@@ -10,7 +10,23 @@ const ll inf = 1e9+7;
 const ll Inf = 1e18;
 const int MaxN = 4e5+10;
 void solve(){
-
+    ll n,l,r;
+    cin >> n >> l >> r;
+    vector<ll>a;
+    set <int> s;
+    for(ll x = l,i = 1; x <= r && i<=n ; x++,i++){
+        int m = gcd(i,x);
+        if(!s.count(m)){
+            s.insert(m);
+            a.pb(x);
+        }
+    }
+    for(auto i : a){
+        cout << i << " ";
+    }cout << '\n';
+    for(auto i : s){
+        cout << i << " ";
+    }cout << '\n';
 }
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);
